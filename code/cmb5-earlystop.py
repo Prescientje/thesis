e6b9e99477f0e7f755600e37f3da021f44853c89
edwardjh <edwardjh@rose-hulman.edu>
2017-04-22 23:26:30 -0400

    finalize early stop script

--- code/cmb5-earlystop.py
+++ code/cmb5-earlystop.py
@@ -18,12 +18,12 @@ def print_write(s):
     logfile.write("\n")
 
 def print_confusion_vals(con):
-    print_write("Specificity = %0.6f" % get_specificity(con))
     print_write("Miss Rate   = %0.6f" % get_missrate(con))
     print_write("Fallout     = %0.6f" % get_fallout(con))
     print_write("Precision   = %0.6f" % get_precision(con))
     print_write("Recall      = %0.6f" % get_recall(con))
     print_write("Accuracy    = %0.6f" % get_accuracy(con))
+    print_write("Specificity = %0.6f" % get_specificity(con))
 
 script, sample_len, superfactor, test_num, loud = argv
 today = date.today()
@@ -44,12 +44,12 @@ learn_rate = 0.001
 stddeviation = 0.02
 ALPHA = 0.002
 loud = int(loud)
-batchsize = 1000
+batchsize = 8000
 if sample_len < 51:
     batchsize = 8000
 if sample_len < 24:
     batchsize = 80000
-epochs = 20
+epochs = 50
 superfactor = int(superfactor)
 print_write("complex-modelbuilder5 early-stop TEST")
 print_write("This does not save any model files.")
@@ -371,6 +371,7 @@ for k in range(10):
     train_ryi, test_ryi = getSection(yright,k+1)
     train_rzi, test_rzi = getSection(zright,k+1)
     train_routi, test_routi = getSection(outright,k+1)
+    '''
     train_ldata_feeder={lx_place: train_lxi,
                                ly_place: train_lyi,
                                lz_place: train_lzi,
@@ -411,6 +412,7 @@ for k in range(10):
                               ryz_place: test_ryi*test_rzi,
                               rzx_place: test_rzi*test_rxi,
                               rout_place: test_routi}
+    '''
     train_bdata_feeder={lx_place: train_lxi,
                                ly_place: train_lyi,
                                lz_place: train_lzi,
@@ -459,7 +461,7 @@ for k in range(10):
         print_write(" ")
 
     e = 0
-    
+    prev2ce = np.array([100,100])
     while e < epochs:
         #print("epoch %d of %d complex %d" % (e+1,epochs,sample_len))
         #print_write("train len = %d" % len(train_lxi))
@@ -514,35 +516,44 @@ for k in range(10):
                            lout_place: train_louti[perm[batch_range]]}
 
             ix += batchsize
-            sess.run(optimizer_left, feed_dict=lbatch_feed)
-            sess.run(optimizer_right,feed_dict=rbatch_feed)
+            #sess.run(optimizer_left, feed_dict=lbatch_feed)
+            #sess.run(optimizer_right,feed_dict=rbatch_feed)
             sess.run(optimizer_both, feed_dict=bbatch_feed)
 
         #new material
-        bout_vals = sess.run(prediction_both, feed_dict=train_bdata_feeder)
+        (epoch_ce, bout_vals) = sess.run([CE_both2, prediction_both], feed_dict=train_bdata_feeder)
+        (epoch_cet, bout_valst) = sess.run([CE_both2, prediction_both], feed_dict=test_bdata_feeder)
         epoch_acc = accuracy_score(train_bdata_feeder[lout_place].argmax(axis=1),
-                                     bout_vals.argmax(axis=1))
-        print(e, epoch_acc)
+                                   bout_vals.argmax(axis=1))
+        epoch_acct = accuracy_score(test_bdata_feeder[lout_place].argmax(axis=1),
+                                    bout_valst.argmax(axis=1))
+        print_write("%02d -- %02.10f -- %02.10f -- %02.10f -- %02.10f" % (e, epoch_acc,epoch_acct,epoch_ce,epoch_cet))
+        if prev2ce[1] < prev2ce[0] and prev2ce[0] < epoch_cet:
+            # previous CE and one before that show increasing trend
+            print("STOPPED EARLY AT EPOCH %d of %d" % (e,epochs))
+            break
         e += 1
+        prev2ce[1] = prev2ce[0]
+        prev2ce[0] = epoch_cet
 
 
         #if (e+1) % (epochs/5) == 0:
             #print(e,"complex")
-    loutputs_pred = sess.run(prediction_left,   feed_dict=train_ldata_feeder)
-    loutputs_test = sess.run(prediction_left,   feed_dict= test_ldata_feeder)
-    routputs_pred = sess.run(prediction_right,  feed_dict=train_rdata_feeder)
-    routputs_test = sess.run(prediction_right,  feed_dict= test_rdata_feeder)
+    #loutputs_pred = sess.run(prediction_left,   feed_dict=train_ldata_feeder)
+    #loutputs_test = sess.run(prediction_left,   feed_dict= test_ldata_feeder)
+    #routputs_pred = sess.run(prediction_right,  feed_dict=train_rdata_feeder)
+    #routputs_test = sess.run(prediction_right,  feed_dict= test_rdata_feeder)
     boutputs_pred = sess.run(prediction_both,   feed_dict=train_bdata_feeder)
     boutputs_test = sess.run(prediction_both,   feed_dict= test_bdata_feeder)
 
     #calculate Xscore (train/test, Xconfusion matrix (train/test), and Xroc_auc (train/test)
     #for left, right, and both
-    ls = accuracy_score(train_ldata_feeder[lout_place].argmax(axis=1),loutputs_pred.argmax(axis=1))
-    lst = accuracy_score(test_ldata_feeder[lout_place].argmax(axis=1),loutputs_test.argmax(axis=1))
-    lc = confusion_matrix(train_ldata_feeder[lout_place].argmax(axis=1),loutputs_pred.argmax(axis=1))
-    lct = confusion_matrix(test_ldata_feeder[lout_place].argmax(axis=1),loutputs_test.argmax(axis=1))
-    lconfusion_sums += lc
-    lconfusion_sumst += lct
+    #ls = accuracy_score(train_ldata_feeder[lout_place].argmax(axis=1),loutputs_pred.argmax(axis=1))
+    #lst = accuracy_score(test_ldata_feeder[lout_place].argmax(axis=1),loutputs_test.argmax(axis=1))
+    #lc = confusion_matrix(train_ldata_feeder[lout_place].argmax(axis=1),loutputs_pred.argmax(axis=1))
+    #lct = confusion_matrix(test_ldata_feeder[lout_place].argmax(axis=1),loutputs_test.argmax(axis=1))
+    #lconfusion_sums += lc
+    #lconfusion_sumst += lct
     #try:
         #lra = roc_auc_score(train_ldata_feeder[k][lout_place].argmax(axis=1),loutputs_pred.argmax(axis=1))
     #except ValueError:
@@ -552,12 +563,12 @@ for k in range(10):
     #except ValueError:
         #lrat = -1
 
-    rs = accuracy_score(train_rdata_feeder[rout_place].argmax(axis=1),routputs_pred.argmax(axis=1))
-    rst = accuracy_score(test_rdata_feeder[rout_place].argmax(axis=1),routputs_test.argmax(axis=1))
-    rc = confusion_matrix(train_rdata_feeder[rout_place].argmax(axis=1),routputs_pred.argmax(axis=1))
-    rct = confusion_matrix(test_rdata_feeder[rout_place].argmax(axis=1),routputs_test.argmax(axis=1))
-    rconfusion_sums += rc
-    rconfusion_sumst += rct
+    #rs = accuracy_score(train_rdata_feeder[rout_place].argmax(axis=1),routputs_pred.argmax(axis=1))
+    #rst = accuracy_score(test_rdata_feeder[rout_place].argmax(axis=1),routputs_test.argmax(axis=1))
+    #rc = confusion_matrix(train_rdata_feeder[rout_place].argmax(axis=1),routputs_pred.argmax(axis=1))
+    #rct = confusion_matrix(test_rdata_feeder[rout_place].argmax(axis=1),routputs_test.argmax(axis=1))
+    #rconfusion_sums += rc
+    #rconfusion_sumst += rct
 
     bs = accuracy_score(train_bdata_feeder[lout_place].argmax(axis=1),boutputs_pred.argmax(axis=1))
     bst = accuracy_score(test_bdata_feeder[lout_place].argmax(axis=1),boutputs_test.argmax(axis=1))
@@ -588,25 +599,25 @@ for k in range(10):
                  #true  positives is C_{1,1}, 
              #and false positives is C_{0,1}.
 
-    train_scores[k,0] = ls
-    test_scores[k,0] = lst
-    train_scores[k,1] = rs
-    test_scores[k,1] = rst
+    #train_scores[k,0] = ls
+    #test_scores[k,0] = lst
+    #train_scores[k,1] = rs
+    #test_scores[k,1] = rst
     train_scores[k,2] = bs
     test_scores[k,2] = bst
 
-    if lst > maxscoret[0]:
-        maxscoret[0] = lst
+    #if lst > maxscoret[0]:
+        #maxscoret[0] = lst
         #lsaver.save(sess=sess,save_path=lmodelname)
         #print_write("lsaver saved %d" % (k+1))
-    if lst < minscoret[0]:
-        minscoret[0] = lst
-    if rst > maxscoret[1]:
-        maxscoret[1] = rst
+    #if lst < minscoret[0]:
+        #minscoret[0] = lst
+    #if rst > maxscoret[1]:
+        #maxscoret[1] = rst
         #rsaver.save(sess=sess,save_path=rmodelname)
         #print_write("rsaver saved %d" % (k+1))
-    if rst < minscoret[1]:
-        minscoret[1] = rst
+    #if rst < minscoret[1]:
+        #minscoret[1] = rst
     if bst > maxscoret[2]:
         maxscoret[2] = bst
         #bsaver.save(sess=sess,save_path=bmodelname)
@@ -618,14 +629,14 @@ for k in range(10):
     
 
 print_write(" ")
-print_write("Left Train Confusion Vals")
-print_confusion_vals(lconfusion_sums)
-print_write("Left Test Confusion Vals")
-print_confusion_vals(lconfusion_sumst)
-print_write("Right Train Confusion Vals")
-print_confusion_vals(rconfusion_sums)
-print_write("Right Test Confusion Vals")
-print_confusion_vals(rconfusion_sumst)
+#print_write("Left Train Confusion Vals")
+#print_confusion_vals(lconfusion_sums)
+#print_write("Left Test Confusion Vals")
+#print_confusion_vals(lconfusion_sumst)
+#print_write("Right Train Confusion Vals")
+#print_confusion_vals(rconfusion_sums)
+#print_write("Right Test Confusion Vals")
+#print_confusion_vals(rconfusion_sumst)
 print_write("Both Train Confusion Vals")
 print_confusion_vals(bconfusion_sums)
 print_write("Both Test Confusion Vals")
